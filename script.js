fbc1c1c0af0a9f33324584f477f6c3e8b9d612ff
Eduardo Lemes <eduardolemes.2003@gmail.com>
2025-06-02 21:42:19 -0300

    Ajustado metodo de salvar na lista

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 const entradaTarefa = document.getElementById('entradaTarefa');
 const listaTarefas = document.getElementById('listaTarefas');
 
+let tarefas = [];
+
 function adicionarTarefa() {
-    console.log(entradaTarefa.value);
-    localStorage.setItem("tarefa1", entradaTarefa.value);
-    //listaTarefas.value = entradaTarefa.value;
+    let texto = entradaTarefa.value.trim();
+    if (texto != "") {
+        tarefas.push(entradaTarefa.value);
+        let item = document.createElement('li');
+        item.innerHTML = `<span>${entradaTarefa.value}</span>`;
+        entradaTarefa.value = "";
+        listaTarefas.appendChild(item);
+    } else {
+        alert("Tarefa inválida");
+    }
 }
+
+entradaTarefa.addEventListener('keypress', function(tecla) {
+    if(tecla.key === 'Enter') {
+        adicionarTarefa();
+    }
+});
